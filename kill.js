80156cf6c81ffd846f826edf18c4249022d83525
Andre Aboulian <aandre@mit.edu>
2014-11-28 21:31:30 -0500

    search bug fix

--- kill.js
+++ kill.js
@@ -1,12 +1,7 @@
 function blockAndDisplay() {
 	// remove news feed
     var feed = $('[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream]');
-    var message = $('#distracted');
-
-    if (feed.length == 0) {
-        message.remove();
-    } else if (message.length == 0) {
-        message = $('<h1>')
+    var message = $('<h1>')
             .attr('id', 'distracted')
             .text("Don't get distracted by Facebook!")
             .css('font-size', '30px')
@@ -14,7 +9,6 @@ function blockAndDisplay() {
             .css('position', 'relative')
             .css('top', '75px');
         $('[data-location=maincolumn]').append(message);
-     }
 
     feed.children().remove();
     $('.ticker_stream').remove();
@@ -22,15 +16,15 @@ function blockAndDisplay() {
     $('#pagelet_games_rhc').remove();
     $('#pagelet_trending_tags_and_topics').remove();
     $('#pagelet_canvas_nav_content').remove();
-    
+
     // remove chat sidebar
     $('.fbChatSidebar').remove();
-    
+
     // clear right col card
     $('#pagelet_rhc_footer').siblings().remove();
-    
+
     // remove select components of left sidebar
-    $('#pinnedNav').siblings('.homeSideNav').remove();
+    $('#pinnedNav').siblings('.homeSideNav').hide();
 }
 
 $(document).ready(blockAndDisplay);
